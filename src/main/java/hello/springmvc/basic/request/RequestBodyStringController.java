676a47f1bc27b2c7c653bc274e3ecdd490031606
yonguk <wldyddnr@gmail.com>
2023-06-30 02:13:56 +0900

    request-body

--- src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -43,7 +43,7 @@ public class RequestBodyStringController {
     }
 
     @PostMapping("/request-body-string-v3")
-    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) throws IOException {
+    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity){
 
         String messageBody = httpEntity.getBody();
 
